ee3571e0413e7b8d51fc93c5a5f1f3fa4bb3dbb9
liBY900 <ST10465691@rcconnect.edu.za>
2026-04-08 13:24:47 +0200

    Junit

--- src/test.java
+++ src/test.java
@@ -1,7 +1,7 @@
-import org.testng.annotations.Test;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
-import static org.testng.Assert.assertFalse;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class test {
 
